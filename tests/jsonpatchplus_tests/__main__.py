cef5b3d463aeadd0501c3ffbaa395493e8847c36
Elmer Nocon <elmernocon@gmail.com>
2023-05-26 16:23:10 +0800

    test: add test for JsonPathPreprocessor

--- tests/jsonpatchplus_tests/__main__.py
+++ tests/jsonpatchplus_tests/__main__.py
@@ -5,7 +5,10 @@ from typing import Any, Callable, Optional
 import yaml
 from deepdiff import DeepDiff
 
-from jsonpatchplus import patch
+from jsonpatchplus import load, patch
+from jsonpatchplus.ctypes import JsonPatchDocumentLoader
+from jsonpatchplus.loaders.jsonpatchplus import Loader
+from jsonpatchplus.preprocessors.jsonpath import JsonPathPreprocessor
 
 
 @cache
@@ -18,11 +21,24 @@ def load_data_file(name: str) -> Any:
     return data
 
 
-def test_patch(orig_file: str, patch_file: str, result_file: str) -> None:
+@cache
+def load_patch_file(name: str, loader: Optional[JsonPatchDocumentLoader] = None) -> Any:
+    test_dir = Path(__file__).parent
+    data_dir = test_dir / "data"
+    data_path = data_dir / name
+    data = load(data_path, loader=loader)
+    return data
+
+
+def test_patch(orig_file: str, patch_file: str, result_file: str, loader: Optional[JsonPatchDocumentLoader] = None) -> None:
     a = load_data_file(orig_file)
-    b = load_data_file(patch_file)
+    if loader:
+        setattr(loader, "doc", a)
+    b = load_patch_file(patch_file, loader=loader)
+    if loader:
+        print(loader.logs)
     c = load_data_file(result_file)
-    d = patch(a, b, None)
+    d = patch(a, b, loader=loader)
     e = DeepDiff(c, d)
     assert not e, e
 
@@ -58,6 +74,13 @@ def main():
         test_patch, "list.yaml", "patch2.yaml", "result2.yaml",
         exception_class_name="JsonPatchInvalidError",
     )
+
+    plus_loader = Loader(
+        preprocessors=[
+            JsonPathPreprocessor()
+        ]
+    )
+    test_patch("deeplist.yaml", "patch3.yaml", "result3.yaml", loader=plus_loader)
 
 
 if __name__ == "__main__":
